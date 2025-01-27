68bcbf7e4814efe1690261423a1b7ef7c8a14d9c
TamplierX <163639339+TamplierX@users.noreply.github.com>
2025-01-27 13:06:44 +0300

    update

--- cookbook_frontend/src/components/App.jsx
+++ cookbook_frontend/src/components/App.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import {Routes, Route } from "react-router";
 
 import Header from "./Header.jsx";
-import Home from "./Home.jsx";
-import Category from "./Category.jsx";
-import Recipes from "./Recipes.jsx";
+import CategoryList from "./CategoryList.jsx";
+import RecipeList from "./RecipeList.jsx";
+import RecipeDetail from "./RecipeDetail.jsx";
 
 import '../styles/App.css';
 
@@ -15,14 +15,14 @@ function App() {
             <div className="main-container">
                 <Header />
                 <Routes>
-                    <Route path="*" element={<Home />} />
+                    <Route path="*" element={<CategoryList />} />
                     <Route
                         path="/category/:categoryId"
-                        element={<Category />}
+                        element={<RecipeList />}
                     />
                     <Route
                         path="/category/:categoryId/recipes/:recipeId"
-                        element={<Recipes />}
+                        element={<RecipeDetail />}
                     />                       
                 </Routes>
             </div>
